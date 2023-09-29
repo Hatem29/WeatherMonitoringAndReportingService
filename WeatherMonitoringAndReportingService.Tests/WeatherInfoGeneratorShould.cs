3930cdaa0f80b8e5871861e900e829e53288b8a9
Hatem29 <hatm.h.s2011@hotmail.com>
2023-09-29 21:14:16 +0300

    WeatherInfoGenerator new test class with a test to check that it chooses the right handler for the data.

---
+++ WeatherMonitoringAndReportingService.Tests/WeatherInfoGeneratorShould.cs
@@ -0,0 +1,41 @@
+﻿using Xunit;
+using AutoFixture;
+using WeatherMonitoringAndReportingService;
+using System;
+using Moq;
+
+namespace WeatherMonitoringAndReportingService.Tests
+{
+    public class WeatherInfoGeneratorShould
+    {
+        public WeatherInfoGeneratorShould()
+        {
+            sut = new WeatherInfoGenerator("");
+            mochHandler = new Mock<IWeatherDataHandler>();
+        }
+        WeatherInfoGenerator sut;
+        Mock<IWeatherDataHandler> mochHandler;
+
+        [Theory]
+        [InlineData(@"{
+    ""Location"": ""City Name"",
+    ""Temperature"": 23.0,
+    ""Humidity"": 85.0
+}", typeof(WeatherJsonHandler))]
+
+        [InlineData(@"<WeatherData>
+  <Location>City Name</Location>
+  <Temperature>23.0</Temperature>
+  <Humidity>85.0</Humidity>
+</WeatherData>", typeof(WeatherXmlHandler))]
+        public void HaveTheRightDataHandlerForTheFormat(string rawData, Type expectedType)
+        {
+            sut.RawData = rawData;
+
+            var weatherInfo = sut.GenerateWetherInfo();
+            var dataHandler = sut.weatherDataHandler;
+
+            Assert.IsAssignableFrom(expectedType, dataHandler);
+        }
+    }
+}
